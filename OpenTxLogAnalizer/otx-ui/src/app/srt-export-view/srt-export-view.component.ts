6e9c4af710187c9bf0080d8d1513f8c5c2a33822
maxbl4 <maxbl4@gmail.com>
2022-02-11 21:48:20 +0300

    OSD Template usage

--- OpenTxLogAnalizer/otx-ui/src/app/srt-export-view/srt-export-view.component.ts
+++ OpenTxLogAnalizer/otx-ui/src/app/srt-export-view/srt-export-view.component.ts
@@ -1,14 +1,36 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {OsdItems, SrtGenerator} from "../../services/srt-generator";
 import {PersistenceService} from "../../services/persistence.service";
 import {DataManager} from "../../services/data-manager";
 import {AssGenerator, LayoutSettings} from "../../services/ass-generator.service";
-import {ILog} from "../../services/open-tx-log-parser";
 import {GpxGenerator} from "../../services/gpx-generator";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'otx-srt-export-view',
   template: `
+    <ng-template #usageInfo let-modal>
+      <div class="modal-header">
+        <h4 class="modal-title" id="modal-basic-title">OSD Template Format</h4>
+        <button type="button" class="close btn btn-danger btn-sm" aria-label="Close" (click)="modal.dismiss('Cross click')">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <h3>Basic</h3>
+        <p>Any text output as is. To output telemtry value put it's name in curly braces: {{'{gps}'}} will output GPS coordinates</p>
+        <p>Example template: <code>Current {{'{current}'}}A</code> will draw something like this: <code>Current 15.1A</code></p>
+        <h3>Value formatting</h3>
+        <p>You can add left padding to the values, so they don't jump around because of different length: <code>{{'{rxBattery,6}'}}</code> will draw <code>"  16.1"</code> and pad with additional 2 spaces</p>
+        <h3>Value bars</h3>
+        <p>You can draw value as bar instead of number <code>{{'{power,5,bar}'}}</code> will draw bar of 5 characters <code>###--</code> representing the current value relative to minimum and maximum in the log. <code>barReverse</code> will draw bar right-to-left.</p>
+        <h3>Header</h3>
+        <p><code>#!x:10,y:720,width:1280,height:720,font:Courier New,fontSize:28,color:ffffff</code></p>
+        <p>All fields are optional, you can remove header if not used</p>
+        <h3>Available fields</h3>
+        <p>gps, lat, lon, gpsSpeed, Sats, distanceToHome, distanceTraveled, rss1, rss2, rqly, rqlySum, rqlyOsd, rsnr, rfmd, tpwr, rxBattery, current, power, capacity, wattPerKm, estimatedRange, estimatedFlightTime, batteryPercent, pitchDeg, rollDeg, yawDeg, throttle, djiDelay, djiBitrate</p>
+      </div>
+    </ng-template>
     <div class="container-fluid">
       <h3>Export subtitles file with OSD telemetry</h3>
       <div class="row mb-3">
@@ -22,7 +44,7 @@ import {GpxGenerator} from "../../services/gpx-generator";
       <div class="row">
         <div class="col">
           <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">OSD Template</label>
+            <label for="exampleFormControlTextarea1" class="form-label">OSD Template</label><button class="btn btn-sm btn-danger ms-2" (click)="showHelp()">Help me!</button>
             <textarea class="form-control osd-layout" id="exampleFormControlTextarea1" rows="15" [(ngModel)]="osdLayout" (keyup)="updatePreview()"></textarea>
           </div>
         </div>
@@ -39,9 +61,6 @@ import {GpxGenerator} from "../../services/gpx-generator";
                      [style.font-family]="layoutSettings.font+'!important'" [style.font-size]="layoutSettings.fontSize+'px'">{{osdLayoutPreview}}</pre>
             </div>
           </div>
-        </div>
-        <div class="col-auto">
-          Available Fields: gps, lat, lon, gpsSpeed, Sats, distanceToHome, distanceTraveled, rss1, rss2, rqly, rqlySum, rqlyOsd, rsnr, rfmd, tpwr, rxBattery, current, power, capacity, wattPerKm, estimatedRange, estimatedFlightTime, batteryPercent, pitchDeg, rollDeg, yawDeg, throttle, djiDelay, djiBitrate
         </div>
       </div>
       <div class="row">
@@ -64,6 +83,9 @@ import {GpxGenerator} from "../../services/gpx-generator";
   ]
 })
 export class SrtExportViewComponent implements OnInit {
+  @ViewChild('usageInfo')
+  usageInfo?: ElementRef;
+
   osdItems: OsdItems = {
     gps: true,
     dist: true,
@@ -83,7 +105,8 @@ export class SrtExportViewComponent implements OnInit {
 `;
   layoutSettings = new LayoutSettings();
 
-  constructor(public data: DataManager, private srtGenerator: SrtGenerator, private assGenerator: AssGenerator, private gpxGenerator: GpxGenerator, private persistance: PersistenceService) { }
+  constructor(public data: DataManager, private srtGenerator: SrtGenerator, private assGenerator: AssGenerator, private gpxGenerator: GpxGenerator, private persistance: PersistenceService,
+              private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.osdItems = this.persistance.srtExport_osdItems ?? this.osdItems;
@@ -129,5 +152,9 @@ export class SrtExportViewComponent implements OnInit {
     a.download = `${this.data.currentLogProject?.otx.name.substring(0, this.data.currentLogProject?.otx.name.length - 4)}.gpx`;
     a.click();
     URL.revokeObjectURL(objectUrl);
+  }
+
+  showHelp() {
+    this.modalService.open(this.usageInfo, {ariaLabelledBy: 'modal-basic-title'});
   }
 }
