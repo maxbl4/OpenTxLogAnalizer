f62d924968b5b461271c985870bdc763c1e76fe0
maxbl4 <maxbl4@gmail.com>
2022-02-13 21:48:11 +0300

    Cool bars

--- OpenTxLogAnalizer/otx-ui/src/services/ass-generator.service.ts
+++ OpenTxLogAnalizer/otx-ui/src/services/ass-generator.service.ts
@@ -1,13 +1,14 @@
 import {Injectable} from "@angular/core";
 import {ILog, Log} from "./open-tx-log-parser";
 import {Duration} from "luxon";
-import {IStatTriple, StatTriple} from "./IStats";
+import {StatTriple} from "./IStats";
 import {knownStats} from "../app/map-view/map-view.component";
 
 const osdHeaderRegex = /^#!.+?\n/gm;
 
 @Injectable()
 export class AssGenerator {
+  private lastMaximum: any = {};
   exportAss(log: ILog, osdLayout: string): string {
     const settings = this.getLayoutSettings(osdLayout);
     const color = reverseColor(settings.color);
@@ -23,6 +24,7 @@ Style: OSD, ${settings.font},${settings.fontSize},&H00${color},&H00${color},&H00
 [Events]
 Format: Marked, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
 `;
+    this.lastMaximum = {};
     let prevTimecode = Duration.fromMillis(0);
     for (let i = 0; i < log.rows.length; i++) {
       const r = log.rows[i];
@@ -68,12 +70,49 @@ Format: Marked, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
       const field = m[1];
       const padding = parseInt(m[2] ?? "0");
       const format = m[3] ?? "number";
-      const value = formatValue(field, row[field], padding, format, log);
+      const value = this.formatValue(field, row[field], padding, format, log);
       osd += osdLayout.substring(prevIndex, m.index) + value;
       prevIndex = m.index! + m[0].length;
     }
     if (prevIndex < osdLayout.length) osd += osdLayout.substring(prevIndex, osdLayout.length);
     return osd;
+  }
+
+  formatValue(field: string, value: any, padding: number, format: string, log: ILog) {
+    switch (format) {
+      case "barReverse":
+      case "bar":
+        let lastMax = this.lastMaximum[field] ?? 0;
+        lastMax--;
+        this.lastMaximum[field] = lastMax;
+        const statDef = knownStats.find(x => x.field == field);
+        const statData = <StatTriple>(<any>log.stats)[field];
+        let normalizedValue = (value - statData.min)/statData.range;
+        if (statDef?.invertOsdBar)
+          normalizedValue = 1 - normalizedValue;
+        const paddedValue = Math.round(normalizedValue * padding);
+        if (lastMax < paddedValue) this.lastMaximum[field] = lastMax = paddedValue;
+        // if (lastMax > paddedValue)
+        //   debugger;
+        let res = "";
+        // if (format == "barReverse")
+        // {
+        //   return "-".repeat(padding - paddedValue) + "#".repeat(paddedValue);
+        // }
+        for (let i = 0; i < padding; i++)
+        {
+          if (i < paddedValue) res += "#";
+          else {
+            if (i == lastMax - 1)
+              res += "|";
+            else res += "-"
+          }
+        }
+        //return "#".repeat(paddedValue) + "-".repeat(padding - paddedValue);
+        return res;
+      default:
+        return pad(value, padding);
+    }
   }
 
   getPreview(osdLayout: string, log: Log) {
@@ -101,26 +140,6 @@ function pad(s?: any, n?: number, symbol: string = " ") {
 function reverseColor(rgb:string) {
   if (rgb.length != 6) return rgb;
   return rgb.substring(4,6) + rgb.substring(2,4) + rgb.substring(0,2);
-}
-
-function formatValue(field: string, value: any, padding: number, format: string, log: ILog) {
-  switch (format) {
-    case "barReverse":
-    case "bar":
-      const statDef = knownStats.find(x => x.field == field);
-      const statData = <StatTriple>(<any>log.stats)[field];
-      let normalizedValue = (value - statData.min)/statData.range;
-      if (statDef?.invertOsdBar)
-        normalizedValue = 1 - normalizedValue;
-      const paddedValue = Math.round(normalizedValue * padding);
-      if (format == "barReverse")
-      {
-        return "-".repeat(padding - paddedValue) + "#".repeat(paddedValue);
-      }
-      return "#".repeat(paddedValue) + "-".repeat(padding - paddedValue);
-    default:
-      return pad(value, padding);
-  }
 }
 
 export interface ILayoutSettings {
